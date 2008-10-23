4d060d17010183bcb49711274c075be49af556fc
Caroline <johnston@biochem.ucl.ac.uk>
2008-10-23 12:52:29 +0100

    Mash1 chipseq initial data analysis

---
+++ chipseq-analysis1.R
@@ -0,0 +1,329 @@
+data<-read.csv("ChIPSeq.csv", skip=1, na.strings='N.A.')
+
+#> colnames(data)
+# [1] "Cluster.ID"                                      
+# [2] "Cluster.Overlap.Count"                           
+# [3] "Cluster.Location"                                
+# [4] "Cluster.Span"                                    
+# [5] "Overlap.Location"                                
+# [6] "Overlap.Span"                                    
+# [7] "Known.Gene.Left.Target"                          
+# [8] "Known.Gene.Left.Target.Gene.Name"                
+# [9] "Known.Gene.Left.Distance.To.Target"              
+#[10] "Known.Gene.Left.Target.Exon"                     
+#[11] "Known.Gene.Left.Target.Location"                 
+#[12] "Known.Gene.Left.Target.Span"                     
+#[13] "Known.Gene.Left.Target.Strand"                   
+#[14] "Binding.Site.Location.to.Known.Gene.Left.Target" 
+#[15] "Known.Gene.Right.Target"                         
+#[16] "Known.Gene.Right.Target.Gene.Name"               
+#[17] "Known.Gene.Right.Distance.To.Target"             
+#[18] "Known.Gene.Right.Target.Exon"                    
+#[19] "Known.Gene.Right.Target.Location"                
+#[20] "Known.Gene.Right.Target.Span"                    
+#[21] "Known.Gene.Right.Target.Strand"                  
+#[22] "Binding.Site.Location.to.Known.Gene.Right.Target"
+ 
+#> dim(data)
+#[1] 8418   22
+
+
+#####
+#write a bed file of the locations
+#####
+
+write(paste('track name="ChIP-seq TC" description="ChIP-seqTC binding"',"\n"), file="seqs.bed")
+
+tmp<-as.character(data$Cluster.Location)
+chip.seq.targets<-cbind(chr=gsub('(.+):.*','\\1', tmp),
+                       start = gsub('.+:(.+)-.*','\\1', tmp),
+                       end = gsub('.+-(.+)','\\1', tmp),
+                        name = paste('"',as.character(data$Cluster.Location),'"', sep=""),
+                        score = data$Cluster.Overlap.Count
+                        )
+
+bed<-apply(chip.seq.targets, 1, function(x){
+                                   x<-paste(x,collapse=" ");
+                                   return(x)
+                                 }
+           )
+write.table(bed, file="seqs.bed", quote=FALSE,append=TRUE, col.names=FALSE, row.names=FALSE)
+
+
+####
+#
+####
+
+left.targets <- unique(as.character(data$Known.Gene.Left.Target))
+right.targets <- unique(as.character(data$Known.Gene.Right.Target))
+left.targets <- left.targets[which(left.targets!="No annotation yet")]
+right.targets <- right.targets[which(right.targets!="No annotation yet")]
+
+#> length(left.targets)
+#[1] 4409
+#> length(right.targets)
+#[1] 4389
+
+
+write.table(left.targets, file="left_targets.csv", quote=FALSE, sep=",", col.names=FALSE, row.names=FALSE)
+write.table(right.targets, file="right_targets.csv", quote=FALSE, sep=",", col.names=FALSE, row.names=FALSE)
+
+intersect.targets <- intersect(left.targets,right.targets)
+write.table(intersect.targets, file="intersect.targets.csv", quote=FALSE, sep=",", col.names=FALSE, row.names=FALSE)
+
+#> length(intersect.targets)
+#[1] 2642
+
+
+# target is gene symbol, may be 'No annotation yet' 
+# Name is refseq, undef for those with no annot
+
+
+#same thing but only for those which have a refseq
+left.data<-data[grep('NM_*', data$Known.Gene.Left.Target.Gene.Name),]
+right.data<-data[grep('NM_*', data$Known.Gene.Right.Target.Gene.Name),]
+
+# > dim(left.data)
+# [1] 5051   22
+# > dim(right.data)
+# [1] 4676   22
+
+left.targets.rs <- unique(as.character(left.data$Known.Gene.Left.Target))
+right.targets.rs <- unique(as.character(right.data$Known.Gene.Right.Target))
+intersect.targets.rs <- intersect(left.targets.rs,right.targets.rs)
+
+
+#not sure how else to do this:
+left.rs <- character(length(left.targets.rs))
+right.rs <- character(length(right.targets.rs))
+intersect.rs <- character(length(intersect.targets.rs))
+
+for (i in 1:length(left.targets.rs)) {
+  left.rs[i] <- as.character(left.data[ which(left.data$Known.Gene.Left.Target == left.targets.rs[i]), 'Known.Gene.Left.Target.Gene.Name'][1])
+  if(is.na(left.rs[i])) {stop(paste("erm: ",i)) }
+}
+                    
+for (i in 1:length(right.targets.rs)) {
+  right.rs[i] <- as.character(right.data[ which(right.data$Known.Gene.Right.Target == right.targets.rs[i]), 'Known.Gene.Right.Target.Gene.Name'][1])
+}
+                    
+            
+for (i in 1:length(intersect.targets.rs)) {
+  intersect.rs[i] <- as.character(right.data[ which(right.data$Known.Gene.Right.Target == intersect.targets.rs[i]), 'Known.Gene.Right.Target.Gene.Name'][1])
+}
+                    
+
+left.targets.rs<-cbind(symbol=left.targets.rs, refseq=left.rs)
+right.targets.rs<-cbind(symbol=right.targets.rs, refseq=right.rs)
+intersect.targets.rs <- cbind(symbol=intersect.targets.rs, refseq=intersect.rs)
+
+#so about half have refseq ids, again - about 60% intragenic
+# > dim(left.targets.rs)
+# [1] 2879    2
+# > dim(right.targets.rs)
+# [1] 2824    2
+# > dim(intersect.targets.rs)
+# [1] 1699    2
+
+
+write.table(left.targets.rs, file="left_targets_rs.csv", quote=FALSE, sep=",", col.names=TRUE, row.names=FALSE)
+write.table(right.targets.rs, file="right_targets_rs.csv", quote=FALSE, sep=",", col.names=TRUE, row.names=FALSE)
+write.table(intersect.targets.rs, file="intersect_targets_rs.csv", quote=FALSE, sep=",", col.names=TRUE, row.names=FALSE)
+
+
+
+
+
+
+
+
+
+
+#I presume this bit actually represents the binding
+wid<-data$Overlap.Span
+bitmap(file="overlap_span_dens_dist.png", res=300)
+plot(density(wid))
+dev.off()
+
+#is this just a product of the sequencing read length?
+big.wid <- data$Cluster.Span
+bitmap(file="cluster_span_dens_dist.png", res=300)
+plot(density(big.wid))
+dev.off()
+
+bitmap(file="log_cluster_span_dens_dist.png", res=300)
+plot(density(log10(big.wid)))
+dev.off()
+
+
+
+#what about distance to nearest genes?
+left.dist <- data$Known.Gene.Left.Distance.To.Target
+right.dist <- data$Known.Gene.Right.Distance.To.Target
+
+#remove any that are undef
+left.dist <- left.dist[!is.na(left.dist)]
+right.dist <- right.dist[!is.na(right.dist)]
+
+dist.union <- c(left.dist, right.dist)
+
+#plot
+bitmap(file="distance_to_tss_dens_dist.png", res=300)
+opar <- par(mfrow=c(3,1))
+plot(density(left.dist))
+plot(density(right.dist))
+plot(density(dist.union))
+dev.off()
+
+#plot logged
+bitmap(file="log_distance_to_tss_dens_dist.png", res=300)
+opar <- par(mfrow=c(3,1))
+plot(density(log10(left.dist)))
+plot(density(log10(right.dist)))
+plot(density(log10(dist.union)))
+dev.off()
+
+
+# looks like the data has been filtered:
+
+#> max(data$Cluster.Overlap.Count)
+#[1] 333
+#> min(data$Cluster.Overlap.Count)
+#[1] 9
+
+
+
+
+
+
+
+
+
+
+###
+# Grab the nearest gene, whether left or right, for each location
+##
+
+colnames <- c("Cluster.ID", "Cluster.Overlap.Count", 'Cluster.Location', 'Cluster.Span', 'Overlap.Location', 'Overlap.Span', 'Target', 'Gene.Name', 'Distance.To.Target', 'Target.Exon', 'Target.Location', 'Target.Span', 'Target.Strand', 'Binding.Site.Location.to.Target', 'side')
+
+nearest <- matrix(nrow=nrow(data), ncol=14)
+side <- character(nrow(data))
+colnames(nearest) <- colnames[1:14]
+tmp<-as.matrix(data)
+
+#for (i in 1:nrow(data)){
+#  row <- data[i,]
+#  nearest[i,1:6] <- as.matrix(row)[1:6]
+#
+#  if ( sum( is.na( c(row$Known.Gene.Right.Distance.To.Target, row$Known.Gene.Left.Distance.To.Target)) ) == 2){
+#    nearest[i,7:14] <- NA
+#    side[i] <- 'none'
+#    next
+#  }
+#  
+#  if (is.na(row$Known.Gene.Right.Distance.To.Target)){
+#    nearest[i,7:14] <- tmp[i,7:14]
+#    side[i] <- 'left'
+#    next
+#  }
+#
+#  if (is.na(row$Known.Gene.Left.Distance.To.Target)){
+#    nearest[i,7:14] <- tmp[i,15:22]
+#    side[i] <- 'right'
+#    next
+#  } 
+#
+#  #note, if equal we take the left one. prob just intrageneic
+#  #with dist 0, so it shouldn't matter
+#  if (row$Known.Gene.Left.Distance.To.Target <= row$Known.Gene.Right.Distance.To.Target){
+#    nearest[i,7:14] <- tmp[i,7:14]
+#    side[i] <- 'left'
+#    next
+#  }
+#  
+#  
+#  if (row$Known.Gene.Right.Distance.To.Target < row$Known.Gene.Left.Distance.To.Target){
+#    nearest[i,7:14] <- tmp[i,15:22]
+#    side[i] <- 'right'
+#    next
+#  }
+#
+#}
+#save(nearest, file="nearest.R")
+
+
+
+#this instead
+load("nearest.R")
+
+nearest <- cbind(nearest, side=side)
+write.table(nearest, file="nearest_genes.csv", quote=FALSE, row.names=FALSE, sep=",")
+
+
+#ok, and plot some dists of nearest.
+
+bitmap(file="nearest_dist_to_target.png", res=300)
+plot(density(as.numeric(nearest[,"Distance.To.Target"])))
+dev.off()
+
+bitmap(file="nearest_log_dist_to_target.png", res=300)
+plot(density(log10(as.numeric(nearest[,"Distance.To.Target"]))))
+dev.off()
+
+
+#and write a file of the 
+
+nearest.gene.symbols<- nearest[,"Target"]
+
+#> length(nearest.gene.symbols)
+#[1] 8418
+#> length(unique(nearest.gene.symbols))
+#[1] 4543
+
+nearest.accessions <- nearest[,"Gene.Name"]
+#> length(nearest.accessions)
+#[1] 8418
+#> length(unique(nearest.accessions))
+#[1] 4788
+
+#not quite sure what this means. accessions are diff transcripts maybe,
+#which would have the same symbol
+
+#why do we have about half the names duplicated
+
+
+write.table(nearest.gene.symbols, file="nearest_gene_symbols.csv", quote=FALSE, row.names=FALSE, sep=",")
+write.table(nearest.accessions, file="nearest_accessions.csv", quote=FALSE, row.names=FALSE, sep=",")
+
+
+# everything has a gene symbol but there are lots of duplicates.
+# accessions are duplicated too.
+
+
+
+#which have big cluster numbers
+
+big.clusters <- nearest[as.numeric(nearest[,"Cluster.Overlap.Count"])>100,]
+
+#> big.clusters[,"Target"]
+# [1] "Lrrc4c"        "Lrrc4c"        "Lrrc4c"        "Lrrc4c"       
+# [5] "Stmn2"         "Tm4sf1"        "Tmem68"        "Cdk5rap2"     
+# [9] "6330530A05Rik" "Rpl26"         "Cdk8"          "BC031151"     
+#[13] "Chl1"          "4930433N12Rik" "4930433N12Rik" "4930433N12Rik"
+#[17] "4930433N12Rik" "4930433N12Rik" "Rpusd4"        "9230019H11Rik"
+#[21] "Raet1a"        "Axin2"         "Mamdc1"        "Kcnh5"        
+#[25] "Chrm3"         "BC055107"      "LOC432825"     "Sh2d4b"       
+#[29] "Ly6c"          "Zc3h7a"        "Pgk2"          "Crem"         
+#[33] "AK013700"      "4930428D18Rik" "Pak3"    
+
+
+
+
+# try some ontology analysis on the genes close to the peaks?
+# Actualy, geometric test isn't really what we want. can't we
+# corrolate distance to tss with ontology term?
+
+# NEED INTERNET!
+
+
+
