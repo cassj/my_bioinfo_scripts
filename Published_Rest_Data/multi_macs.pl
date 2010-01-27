6169ff29d71893f979cb553f96a3b231b7524aec
Cass Johnston <cassj@clive.(none)>
2010-01-27 14:02:38 +0000

    changes to multi_macs

--- Published_Rest_Data/multi_macs.pl
+++ Published_Rest_Data/multi_macs.pl
@@ -9,6 +9,8 @@ use Cwd;
 my $cmd = 'macs';
 my $treatment = $ARGV[0];
 my $control = $ARGV[1];
+my $outdir = $ARGV[2] || '.';
+
 my $genome_size = 2700000000; #this is the macs default anyway
 
 #variables:
@@ -21,18 +23,20 @@ my $count = 1;
 my $readme = new IO::File;
 $readme->open('>README') or die "Can't open file README for writing";
 
+my $dir = getcwd;
+
 #for each combinations of variable parameters, run macs.
 foreach my $bw (@bandwidth){
     foreach my $mfold (@mfold){
         foreach my $pvalue (@pvalue){
+
             print "Running for BW: $bw ; MFOLD: $mfold ;  PVAL: $pvalue\n";
 
-            print $readme "run$count\tmacs -t '../$treatment' -c '../$control' --gsize $genome_size --bw $bw --mfold $mfold --pvalue $pvalue --format BED\n";
+            print $readme "$outdir/run$count\tmacs -t '$dir/$treatment' -c '$dir/$control' --gsize $genome_size --bw $bw --mfold $mfold --pvalue $pvalue --format BED\n";
 
-            system("mkdir run$count");
-            my $dir = getcwd;
-            chdir("run$count");
-            my $out = `macs -t '../$treatment' -c '../$control' --gsize $genome_size --bw $bw --mfold $mfold --pvalue $pvalue --format BED  2>&1`;
+            system("mkdir $outdir/run$count");
+            chdir("$outdir/run$count");
+            my $out = `macs -t '$dir/$treatment' -c '$dir/$control' --gsize $genome_size --bw $bw --mfold $mfold --pvalue $pvalue --format BED  2>&1`;
             my $outfh = new IO::File;
             $outfh->open(">out.txt");
             print $outfh $out;
@@ -41,8 +45,8 @@ foreach my $bw (@bandwidth){
             chdir($dir);
 
             #pack up the results so we don't run out of space.
-            system("tar -czf run$count.tgz run$count");
-            system("rm -Rf run$count");
+            #system("tar -czf $outdir/run$count.tgz $outdir/run$count");
+            #system("rm -Rf $outdir/run$count");
 
             $count++;
 
