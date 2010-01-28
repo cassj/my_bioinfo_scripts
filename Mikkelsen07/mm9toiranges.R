706a8f574cb27c2e704921e778d0df8a903d9dec
Cass Johnston <cassj@clive.(none)>
2010-01-28 16:19:03 +0000

    inexplicable foo

--- Mikkelsen07/mm9toiranges.R
+++ Mikkelsen07/mm9toiranges.R
@@ -10,7 +10,7 @@ data <- read.csv(filename, header=F, sep="\t")
 colnames(data) <- qw(Chr, Start, End, Name, Score)
 
 #we have to give them names to avoid a bug in ChIPpeakAnnot if we want to use it later
-data[,"Name"]<-paste(paste(data[,"Chr"],foo[,"Start"],sep=":"),data[,"End"], sep="-")
+data[,"Name"]<-paste(paste(data[,"Chr"],data[,"Start"],sep=":"),data[,"End"], sep="-")
 
 #some inexpliably have a row of NA at the bottom.
 data<-data[!is.na(data[,"Chr"]),]
