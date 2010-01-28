47cca15734edd10ced41af0c470651238ba82dc4
Cass Johnston <cassj@clive.(none)>
2010-01-28 13:09:06 +0000

    fix chr names in hmm bed files (Mikkelsen)

--- Mikkelsen07/hmm2bed.R
+++ Mikkelsen07/hmm2bed.R
@@ -6,5 +6,8 @@ eval(parse(text=args[grep('filename', args)]))
 #read it in, don't write it out
 data <- read.csv(filename, header=T, sep="\t")
 
+#and for consistency, we want the chr names to be in the form chrN
+data[,1]<-paste('chr',data[,1], sep="")
+
 outfile=sub('.txt','.mm8.bed', sub('publication','results', filename))
 write.table(data, file=outfile, sep="\t", col.names=F, row.names=F, quote=F)
