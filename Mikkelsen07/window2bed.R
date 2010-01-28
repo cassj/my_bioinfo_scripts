320ba8320668017f5cca90935fd06fd134906ef6
Cass Johnston <cassj@clive.(none)>
2010-01-28 12:23:00 +0000

    scripts for processing Mikkelsen data

---
+++ Mikkelsen07/window2bed.R
@@ -0,0 +1,12 @@
+#call like R --vanilla --args filename=\"thing\"
+args<-commandArgs()
+eval(parse(text=args[grep('filename', args)]))
+
+data <- read.csv(filename, header=F, sep="\t")
+
+#the only reason this isn't bed is that it is missing a name col
+data<-cbind(data[,1:3], rep('',nrow(data)), data[,4])
+
+outfile=sub('.txt','.mm9.bed', sub('publication','results', filename))
+
+write.table(data, file=outfile, sep="\t", col.names=F, row.names=F, quote=F)
