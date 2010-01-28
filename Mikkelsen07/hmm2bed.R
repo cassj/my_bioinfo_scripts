ba187da306e9a7b732931545b744af9f64f5beff
Cass Johnston <cassj@clive.(none)>
2010-01-28 13:05:42 +0000

    convert the hmm Mikkelsen data to bed (mm8)

--- Mikkelsen07/hmm2bed.R
+++ Mikkelsen07/hmm2bed.R
@@ -2,11 +2,9 @@
 args<-commandArgs()
 eval(parse(text=args[grep('filename', args)]))
 
-data <- read.csv(filename, header=F, sep="\t")
-
-#the only reason this isn't bed is that it is missing a name col
-data<-cbind(data[,1:3], rep('',nrow(data)), data[,4])
+#the only reason this isn't bed is that it has colnames
+#read it in, don't write it out
+data <- read.csv(filename, header=T, sep="\t")
 
 outfile=sub('.txt','.mm8.bed', sub('publication','results', filename))
-
 write.table(data, file=outfile, sep="\t", col.names=F, row.names=F, quote=F)
--- Mikkelsen07/mm2tomm9.R
+++ Mikkelsen07/mm2tomm9.R
@@ -1,3 +1,5 @@
+source("scripts/liftOver.R")
+
 #call like R --vanilla --args filename=\"thing\"
 args<-commandArgs()
 eval(parse(text=args[grep('filename', args)]))
