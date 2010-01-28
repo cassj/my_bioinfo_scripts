b089485fd2e3d43bb9d103f4552d20ba1e9c4fba
Cass Johnston <cassj@clive.(none)>
2010-01-28 15:48:39 +0000

    changes to Mikkelsen bed files

--- Mikkelsen07/hmm2bed.R
+++ Mikkelsen07/hmm2bed.R
@@ -9,5 +9,8 @@ data <- read.csv(filename, header=T, sep="\t")
 #and for consistency, we want the chr names to be in the form chrN
 data[,1]<-paste('chr',data[,1], sep="")
 
+#and the same num of cols as the windowinterval data
+data<-cbind(data,name=NULL, score=NULL)
+
 outfile=sub('.txt','.mm8.bed', sub('publication','results', filename))
 write.table(data, file=outfile, sep="\t", col.names=F, row.names=F, quote=F)
