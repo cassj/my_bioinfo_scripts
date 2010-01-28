cd99909216e613fbb3d2735bbb5c345af6bf7156
Cass Johnston <cassj@clive.(none)>
2010-01-28 16:14:42 +0000

    typo

--- Mikkelsen07/window2bed.R
+++ Mikkelsen07/window2bed.R
@@ -5,7 +5,7 @@ eval(parse(text=args[grep('filename', args)]))
 data <- read.csv(filename, header=F, sep="\t")
 
 #the only reason this isn't bed is that it is missing a name col
-data<-cbind(data[,1:3], name=NULL, data[,4])
+data<-cbind(data[,1:3], name=rep(NULL,nrow(data)), data[,4])
 
 outfile=sub('.txt','.mm8.bed', sub('publication','results', filename))
 
