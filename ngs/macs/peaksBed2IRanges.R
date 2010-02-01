6c4dfe1563a6a8f72b0fa2d0f951a7845fb40cd5
Cass Johnston <cassj@clive.(none)>
2010-02-01 14:00:52 +0000

    convert Macs peak finding output to BioC IRanges  RangedData

---
+++ ngs/macs/peaksBed2IRanges.R
@@ -0,0 +1,41 @@
+source("scripts/qw.R")
+library(IRanges)
+
+#call like R --vanilla --args filename=\"thing\"
+args<-commandArgs()
+eval(parse(text=args[grep('filename', args)]))
+
+skip <- 17
+if( length(grep('negative', filename))>0 ){
+ skip <- 0
+}
+
+data <- read.csv(filename, header=T, sep="\t", skip=skip)
+
+colnames(data) <- qw(Chr, Start, End, Length, Summit, nTags, neg10log10pVal, FoldEnrichment)
+values <-  qw(Length, Summit, nTags, neg10log10pVal, FoldEnrichment)
+
+#sometimes they have FDR
+if(ncol(data)==9){
+  colnames(data)[9] <- "FDR"
+  values<-c(values,"FDR")
+}
+
+#we have to give them names to avoid a bug in ChIPpeakAnnot if we want to use it later
+data[,"Name"]<-paste(paste(data[,"Chr"],data[,"Start"],sep=":"),data[,"End"], sep="-")
+
+
+rd <- RangedData(ranges = IRanges(
+                   start= data$Start,
+                   end = data$End,
+                   ),
+                 names = as.character(data$Name),
+                 space = as.character(data$Chr),
+                 values = data[,values]
+                 )
+
+outfile=sub('.xls','.RangedData.R',filename)
+
+#And save the result
+save(rd, file=outfile)
+
