44dc04222ad7043c7a747307ba3b45366203b092
Cass Johnston <cassj@clive.(none)>
2010-02-05 17:26:35 +0000

    annotation of Ranged Data wiht mm9RDtoGenes

--- ngs/mm9RDtoGenes.R
+++ ngs/mm9RDtoGenes.R
@@ -3,10 +3,17 @@ library(IRanges)
 #call like R --vanilla --args filename=\"thing\"
 args<-commandArgs()
 eval(parse(text=args[grep('filename', args)]))
+load(filename)
 
 library(ChIPpeakAnno)
 
 #load mouse transcripts
 data(TSS.mouse.NCBIM37)
 
-#
+#need to alter the names so they match ChIPpeakAnno
+names(rd)<-gsub("chr","",names(rd))
+
+data.annot <- annotatePeakInBatch(rd, AnnotationData=TSS.mouse.NCBIM37)
+
+newfile<-sub("RangedData", "AnnoRangedData", filename)
+save(data.annot, file=newfile)
