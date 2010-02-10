0f49a1fae2a5078d39c2c91eceb7b64139e60710
Cass Johnston <cassj@clive.(none)>
2010-02-10 11:19:00 +0000

    adding output directory option to multi_macs and removing the tgz for now. Probably needs Getopt now.

--- ngs/macs/multi_macs.pl
+++ ngs/macs/multi_macs.pl
@@ -6,15 +6,14 @@ use warnings;
 use IO::File;
 use Cwd;
 
-my $cmd = 'macs';
-my $R = 'R';
 
+#gah. Getopt, when I have time.
+my $cmd = 'macs';
 my $treatment = $ARGV[0];
 my $control = $ARGV[1];
-chomp $treatment if $treatment;
-chomp $control if $control;
+my $outdir = $ARGV[2] || getcwd;
 
-my $genome_size = 2394590051; #70% of mm9, just a guess.
+my $genome_size = 2700000000; #this is the macs default anyway
 
 #variables:
 my @bandwidth = (100, 200, 300);
@@ -26,34 +25,42 @@ my $count = 1;
 my $readme = new IO::File;
 $readme->open('>README') or die "Can't open file README for writing";
 
+if (-e $outdir){
+    opendir DIR, $outdir;
+    if(grep !/^\.+$/, readdir(DIR)){
+	die "Directory $outdir exists and is not empty.";
+    }
+
+}else{
+    `mkdir $outdir`;
+}
+
+
+my $dir = getcwd;
+
 #for each combinations of variable parameters, run macs.
 foreach my $bw (@bandwidth){
     foreach my $mfold (@mfold){
         foreach my $pvalue (@pvalue){
+
             print "Running for BW: $bw ; MFOLD: $mfold ;  PVAL: $pvalue\n";
 
-	    my $cmd = "macs -t '../$treatment'";
-	    $cmd .= " -c '../$control'" if $control;
-	    $cmd .= " --gsize $genome_size --bw $bw --mfold $mfold --pvalue $pvalue --format BED --wig";
+            print $readme "$outdir/run$count\tmacs -t '$dir/$treatment' -c '$dir/$control' --gsize $genome_size --bw $bw --mfold $mfold --pvalue $pvalue --format BED\n";
 
-            print $readme "run$count\t$cmd\n";
-	    system("rm -Rf run$count");
-	    system("mkdir run$count");
-            my $dir = getcwd;
-            chdir("run$count");
-
-            my $out = `$cmd  2>&1`;
-
+            `mkdir $outdir/run$count`;
+            chdir("$outdir/run$count");
+            
+            my $out = `macs -t '$dir/$treatment' -c '$dir/$control' --gsize $genome_size --bw $bw --mfold $mfold --pvalue $pvalue --format BED  2>&1`;
             my $outfh = new IO::File;
             $outfh->open(">out.txt");
             print $outfh $out;
             $outfh->close;
-            system("$R --vanilla < NA_model.r 2>&1");
+            system("R --vanilla < NA_model.r 2>&1");
             chdir($dir);
 
             #pack up the results so we don't run out of space.
-            system("tar -czf run$count.tgz run$count");
-            system("rm -Rf run$count");
+            #system("tar -czf $outdir/run$count.tgz $outdir/run$count");
+            #system("rm -Rf $outdir/run$count");
 
             $count++;
 
@@ -62,3 +69,5 @@ foreach my $bw (@bandwidth){
 }
 
 $readme->close;
+
+
