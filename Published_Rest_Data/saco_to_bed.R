790c9c18a3bd8f621015f682d62c7d071273793a
Cass Johnston <cassj@clive.(none)>
2010-02-15 12:30:15 +0000

    remove NA from saco bed file

--- Published_Rest_Data/saco_to_bed.R
+++ Published_Rest_Data/saco_to_bed.R
@@ -6,5 +6,13 @@ eval(parse(text=args[grep('filename', args)]))
 data<- read.table(filename,sep="\t", header=T)
 
 bed <- data[,qw(Chr,Start,End)]
+#remove anything that couldn't map
+inds<-which(is.na(bed[,1]) | is.na(bed[,2]) | is.na(bed[,3]))
+if(length(inds)>0){
+  bed<-bed[-1*inds,]
+}
+
 newfile <- 'results/saco_mm9.bed'
 write.table(bed, file=newfile, sep="\t", col.names=F, row.names=F, quote=F)
+
+
