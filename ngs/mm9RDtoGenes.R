341bac345e340755de6972ecbe90ded7d83d96c7
Cass Johnston <cassj@clive.(none)>
2010-02-17 23:30:08 +0000

    fixed annotation of RangedData for Mash/Ngn study

--- ngs/mm9RDtoGenes.R
+++ ngs/mm9RDtoGenes.R
@@ -1,4 +1,5 @@
 library(IRanges)
+source("scripts/qw.R")
 
 #call like R --vanilla --args filename=\"thing\"
 args<-commandArgs()
@@ -15,6 +16,8 @@ names(rd)<-gsub("chr","",names(rd))
 
 data.annot <- annotatePeakInBatch(rd, AnnotationData=TSS.mouse.NCBIM37)
 
+data.annot <- as.data.frame(data.annot)
+rownames(data.annot) <- as.character(data.annot$names)
 
 #and that only gives you the ensembl gene ID, so get extra info:
 library(biomaRt)
@@ -22,7 +25,7 @@ library(biomaRt)
 ensmart <- useMart("ensembl", dataset="mmusculus_gene_ensembl")
                 
 filters <- c("ensembl_gene_id")
-values<-unique(as.character(values(data.annot)[,"feature"]))
+values<-unique(as.character(data.annot[,"feature"]))
 attributes <- c("ensembl_gene_id","mgi_symbol", "description")
 
 more.annot <- getBM(filters=filters, values=values, attributes=attributes, mart=ensmart)
@@ -35,25 +38,39 @@ more.annot <- more.annot[-1*(which(duplicated(more.annot[,c(1,3)]))),]
 
 rownames(more.annot) <- more.annot[,"ensembl_gene_id"]
 
-#and reset the values data.annot with the extra annotation
-#and the original scores.
-annot <- values(data.annot)
-rd <- values(rd)
-new.annot <- list()
-for(i in 1:length(annot)){
- this.df <- annot[[i]]
- this.rd.df <- rd[[i]]
- these.ids <- this.df[,"feature"]
- this.df<-cbind(this.df,this.rd.df,DataFrame( more.annot[these.ids,2:3]))
- new.annot[[i]] <- DataFrame(this.df)
-}
+#add this extra data to the data.annot
+data.annot <- data.frame(data.annot, more.annot[data.annot[,"feature"],])
+
+#and add all of this to your original rd
+rd<-as.data.frame(rd)
+nms<-as.character(rd$names)
+rownames(rd)<-nms
+data.annot<-data.annot[nms,]   #sort annotation to same order as rd
+rd<-cbind(rd, data.annot) 
+
+colnames(rd)<-gsub( "values.","", colnames(rd))
+colnames(rd)[17] <- "gene.strand"
+colnames(rd)[19] <- "gene.start"
+colnames(rd)[20] <- "gene.end"
+colnames(rd)[21] <- "is.inside.gene"
+colnames(rd)[22] <- "distance.to.gene"
+colnames(rd)[23] <- "ensembl.gene.id"
+colnames(rd)[24] <- "gene.mgi.symbol"
+colnames(rd)[25] <- "gene.description"
+
+values<-qw(Length, Summit, nTags, neg10log10pVal, FoldEnrichment, FDR, gene.strand, gene.start, gene.end, is.inside.gene, distance.to.gene, ensembl.gene.id, gene.mgi.symbol, gene.description) 
+
+rd.annot <- RangedData(ranges = IRanges(
+                                   start= rd$start,
+                                   end = rd$end,
+                                   names = rd$names
+                                ),
+                       space = rd$space,
+                       values = rd[,values]
+                      )
 
 
-new.annot <- SplitDataFrameList(new.annot, compress=T)
-names(new.annot) <- names(annot)
-
-values(data.annot) <- new.annot
 
 
 newfile<-sub("RangedData", "AnnoRangedData", filename)
-save(data.annot, file=newfile)
+save(rd.annot, file=newfile)
