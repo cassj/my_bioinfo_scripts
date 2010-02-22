8a738f02a8850a55966c21d170b28ad4141df5c8
Cass Johnston <cassj@clive.(none)>
2010-02-22 23:45:26 +0000

    fixing colnames

--- Published_Rest_Data/johnsonpetannotate.R
+++ Published_Rest_Data/johnsonpetannotate.R
@@ -13,6 +13,7 @@ data(TSS.mouse.NCBIM37)
 
 #need to alter the names so they match ChIPpeakAnno
 names(rd)<-gsub("chr","",names(rd))
+names(rd)[20]<-"MT"
 
 data.annot <- annotatePeakInBatch(rd, AnnotationData=TSS.mouse.NCBIM37)
 
@@ -23,7 +24,7 @@ rownames(data.annot) <- as.character(data.annot$names)
 library(biomaRt)
 
 ensmart <- useMart("ensembl", dataset="mmusculus_gene_ensembl")
-                
+
 filters <- c("ensembl_gene_id")
 values<-unique(as.character(data.annot[,"feature"]))
 attributes <- c("ensembl_gene_id","mgi_symbol", "description")
@@ -32,7 +33,6 @@ more.annot <- getBM(filters=filters, values=values, attributes=attributes, mart=
 
 #ditch any that don't have a symbol
 more.annot <- more.annot[more.annot[,"mgi_symbol"]!="",]
-
 #and some have more than one symbol, but the same desc, so ditch them
 more.annot <- more.annot[-1*(which(duplicated(more.annot[,c(1,3)]))),]
 
@@ -42,21 +42,18 @@ rownames(more.annot) <- more.annot[,"ensembl_gene_id"]
 ord <- as.character(data.annot[,"feature"])
 data.annot <- data.frame(data.annot, more.annot[ord,])
 
-colnames(data.annot) <- qw(gene.space, gene.start, gene.end, gene.width, gene.names, gene.strand, ensembl.gene.id, gene.start.position, gene.end.position,
-                           peak.inside.gene, distance.to.gene, ensembl.gene.id2, mgi.symbol, description)
 
 #and add all of this to your original rd
 rd<-as.data.frame(rd)
 nms<-as.character(rd$names)
 rownames(rd)<-nms
 data.annot<-data.annot[nms,]   #sort annotation to same order as rd
-rd<-cbind(rd, data.annot) 
+rd<-cbind(rd, data.annot)
 
 colnames(rd)<-gsub( "values.","", colnames(rd))
 
-values <- qw(Length,  Summit, nTags,neg10log10pVal, FoldEnrichment, ensembl.gene.id,gene.strand, gene.start.position, gene.end.position, peak.inside.gene, distance.to.gene, mgi.symbol, description  )
-
-if ("values" %in%  colnames(rd)) {values <- c(values,"FDR")} 
+rm.cols<-qw(start, end, names, space, width)
+values<- colnames(rd)[!colnames(rd) %in%rm.cols]
 
 rd.annot <- RangedData(ranges = IRanges(
                                    start= rd$start,
@@ -72,7 +69,5 @@ rd.annot <- RangedData(ranges = IRanges(
 
 newfile<-sub("RangedData", "AnnoRangedData", filename)
 save(rd.annot, file=newfile)
-
-
 
 
