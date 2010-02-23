8f3667b68e1fcde945a9d37cddd6a85e4a118b98
Cass Johnston <cassj@clive.(none)>
2010-02-23 15:14:58 +0000

    don't keep two copies of the ensembl ID

--- ngs/hg19RDtoGenes.R
+++ ngs/hg19RDtoGenes.R
@@ -22,19 +22,18 @@ if (length(id)>0){
 
 
 #fetch nearest gene annot with ChIPpeakAnno
+library(biomaRt)
+ensmart <- useMart("ensembl", dataset="hsapiens_gene_ensembl")
+
 TSS.human.GRCh37 <- getAnnotation(ensmart, featureType="TSS")
 
 data.annot <- annotatePeakInBatch(rd, AnnotationData=TSS.human.GRCh37)
 data.annot <- as.data.frame(data.annot)
 rownames(data.annot) <- as.character(data.annot$names)
 
-
 #and that only gives you the ensembl gene ID, so get extra info:
-library(biomaRt)
-ensmart <- useMart("ensembl", dataset="hsapiens_gene_ensembl")
-
 filters <- c("ensembl_gene_id")
-values<-unique(as.character(values(data.annot)[,"feature"]))
+values<-unique(data.annot[,"feature"])
 attributes <- c("ensembl_gene_id","hgnc_symbol", "description")
 
 more.annot <- getBM(filters=filters, values=values, attributes=attributes, mart=ensmart)
@@ -44,8 +43,9 @@ more.annot <- more.annot[more.annot[,"hgnc_symbol"]!="",]
 
 #and some have more than one symbol, but the same desc, so ditch them
 more.annot <- more.annot[-1*(which(duplicated(more.annot[,c(1,3)]))),]
-
 rownames(more.annot) <- more.annot[,"ensembl_gene_id"]
+more.annot <- more.annot[,which(colnames(more.annot)!="ensembl_gene_id")]
+
 
 #add this extra data to the data.annot
 ord <- as.character(data.annot[,"feature"])
