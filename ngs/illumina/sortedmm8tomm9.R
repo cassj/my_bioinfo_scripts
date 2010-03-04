1343ebef2f7d88c8e1aa54977ac928c1b6f6cb41
Cass Johnston <cassj@clive.(none)>
2010-03-04 17:27:37 +0000

    fixed stuff brroken by previous commit. Sigh :)

--- ngs/illumina/sortedmm8tomm9.R
+++ ngs/illumina/sortedmm8tomm9.R
@@ -44,7 +44,7 @@ repeat {
   cat(n,"\n")
   n <- n+1
   data <- NULL
-  try({data <- read.table(in.file, sep="\t", header=F, nrow=10000, na.strings="", colClasses=colClasses)}, silent=T)
+  try({data <- read.table(in.file, sep="\t", header=F, nrow=10000, na.strings="")})
   if(is.null(data)) { break }
   colnames(data) <- colnms
   new.data <- liftOver.sorted(data)
