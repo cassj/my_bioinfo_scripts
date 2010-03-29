c5efae3a70539a5a15e5db7934bcdd03ffefc6c2
Cass Johnston <cassj@clive.(none)>
2010-03-29 12:31:24 +0100

    reorder by significance and calculate pval from -10log10p

--- ngs/annoRDtoCSV.R
+++ ngs/annoRDtoCSV.R
@@ -7,6 +7,10 @@ data.annot <- get(load(filename))
 
 data<-as.data.frame(data.annot)
 colnames(data)[1]<-"chr"
-
+colnames(data) <- gsub('values.', '', colnames(data))
+data <- data[order(data$neg10log10pVal, decreasing=T),]
+pval <- 10^(data$neg10log10pVal/-10)
+data <- cbind(data, pval)
+  
 newfile<-sub("\\.R", ".csv", filename)
 write.csv(data, file=newfile, row.names=F)
