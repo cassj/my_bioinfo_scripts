f18217b9b1ef3a6c0cd3584917ace93ea05b56a6
Cass Johnston <cassj@clive.(none)>
2010-04-01 14:19:53 +0100

    make fasta file from peakswithseqs

---
+++ ngs/macs/make_fasta.R
@@ -0,0 +1,18 @@
+#call like R --vanilla --args filename=\"thing\" start=1 end=10 outfile=\"otherthing\"
+args<-commandArgs()
+eval(parse(text=args[grep('filename', args)]))
+eval(parse(text=args[grep('outfile', args)]))
+
+eval(parse(text=args[grep('start', args)]))
+eval(parse(text=args[grep('end', args)]))
+
+data <- read.csv(filename, as.is=T)
+
+#delete any existing version
+unlink(newfile)
+
+for (i in start:end){
+  write(paste('>', data$names[i], "\n", data$sequence[i], sep=""),append=T, file=outfile)
+}
+
+
