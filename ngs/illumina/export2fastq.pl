01631ba4c1e9d3fe85f38ec784095499d8092266
Cass Johnston <cassjohnston@gmail.com>
2010-08-27 14:01:49 +0100

    ignore empty lines in export2fastq

--- ngs/illumina/export2fastq.pl
+++ ngs/illumina/export2fastq.pl
@@ -4,6 +4,8 @@ use warnings;
 
 while(<>){
   chomp;
+  #ignore empty lines etc.
+  next unless /^\w.*/;
   my @parts = split /\t/;
   $parts[8]=~s/\./N/g;
   print "@","$parts[0]:$parts[2]:$parts[3]:$parts[4]:$parts[5]#$parts[6]/$parts[7]\n";
